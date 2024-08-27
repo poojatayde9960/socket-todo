4c30051583b53eaae4973715c40e1bc8484e98cd
pooja <https://github.com/poojatayde9960/ecom.git>
2024-08-27 12:57:13 +0530

    controller chages

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,14 +1,19 @@
 const asyncHandler = require("express-async-handler")
+const Todo = require("../modal/Todo")
 
 exports.createTodo = asyncHandler(async (req, res) => {
+    await Todo.create(req.body)
     res.json({ message: "createTodo success" })
 })
 exports.readTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "readTodo success" })
+    const result = await Todo.find()
+    res.json({ message: "readTodo success", result })
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.findByIdAndUpdate(req.params._id, req.body)
     res.json({ message: "updateTodo success" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id)
     res.json({ message: "deleteTodo success" })
 })
